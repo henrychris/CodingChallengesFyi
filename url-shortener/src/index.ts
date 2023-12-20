3bfca9e988f9d166a528829f11fccca6ca01df2d
henrychris <henrychris2003@gmail.com>
2023-12-20 10:34:46 +0100

    return error or 200 from /shorten

--- url-shortener/src/index.ts
+++ url-shortener/src/index.ts
@@ -15,10 +15,13 @@ app.get("/", (_req, res): void => {
 
 app.post(
     "/shorten",
-    async (req: CustomRequest<ShortenerRequest>, res): Promise<void> => {
+    async (req: CustomRequest<ShortenerRequest>, res) => {
         var createdResponse = await shorten(req.body.longUrl);
-        // todo: create an errorOr type structure or, throw exceptions
-        res.send(createdResponse);
+
+        if (createdResponse) {
+            res.status(200).send(createdResponse);
+        }
+        res.status(400).send({ error: true, message: "Something went wrong." });
     }
 );
 
